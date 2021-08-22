9aeb313c4b643537b6d105c90140695911ca518c
sgtchacald <sgt.chacal.d@gmail.com>
2021-08-21 23:15:40 -0300

    Criado Insert para ativos

--- src/main/java/br/com/diegocordeiro/dscproject/resources/AtivoResource.java
+++ src/main/java/br/com/diegocordeiro/dscproject/resources/AtivoResource.java
@@ -1,11 +1,15 @@
 package br.com.diegocordeiro.dscproject.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.diegocordeiro.dscproject.domain.Ativo;
 import br.com.diegocordeiro.dscproject.services.AtivoService;
@@ -21,6 +25,13 @@ public class AtivoResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Ativo obj = service.buscarPorId(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Ativo> insert(@RequestBody Ativo obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); 
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
--- src/main/java/br/com/diegocordeiro/dscproject/services/AtivoService.java
+++ src/main/java/br/com/diegocordeiro/dscproject/services/AtivoService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.diegocordeiro.dscproject.domain.Ativo;
 import br.com.diegocordeiro.dscproject.domain.Pais;
@@ -20,5 +21,28 @@ public class AtivoService {
 		Optional<Ativo> obj = repositorio.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pais.class.getName()));
 	}
+	
+	@Transactional
+	public Ativo insert(Ativo obj) {
+		obj.setId(null);
+		repositorio.save(obj);
+		return obj;
+	}
+		
+//	public Ativo update(Ativo obj) throws ObjectNotFoundException {
+//		Ativo newObj = find(obj.getId());
+//		updateData(newObj, obj);
+//		repositorio.save(newObj);
+//		return newObj;
+//	}
+//	
+//	public void delete(Integer id) throws ObjectNotFoundException {
+//		repositorio.find(id);
+//		try {
+//			repo.delete(id);
+//		} catch (DataIntegrityViolationException e) {
+//			throw new DataIntegrityException("Não foi possível excluir, pois existem entidades relacionadas, verifique.");
+//		}
+//	}
 
 }
