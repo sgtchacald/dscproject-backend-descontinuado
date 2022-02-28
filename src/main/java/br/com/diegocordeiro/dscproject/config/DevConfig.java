976b26f385b62bde31c07523dc894b8efa70b8d8
sgtchacald <sgt.chacal.d@gmail.com>
2022-02-28 18:54:22 -0300

    Adicionando Perfil Homologacao

--- src/main/java/br/com/diegocordeiro/dscproject/config/DevConfig.java
+++ src/main/java/br/com/diegocordeiro/dscproject/config/DevConfig.java
@@ -1,6 +1,7 @@
 package br.com.diegocordeiro.dscproject.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,9 +14,19 @@ public class DevConfig {
 	@Autowired
 	private DBService dbService;
 	
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+	
 	@Bean
 	public boolean instatiateDatabase(){
+		
+		if(!"create-drop".equals(strategy)){
+			return false;
+		}
+		
 		dbService.instantiateDevDatabase();
+		
 		return true;
 	}
 }
