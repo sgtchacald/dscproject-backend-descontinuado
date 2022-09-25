59bef776e6b63ae274d7974aa4f62b197e73f2ae
sgtchacald <sgt.chacal.d@gmail.com>
2022-09-25 12:15:09 -0300

    Reiniciando Projetos Frontend e do Site

--- site/src/common/template/dependencias.js
+++
@@ -1,8 +0,0 @@
-import "../../../public/css/site.css";
-import "../../../public/css/aos.css";
-import "../../../public/css/bootstrap.css";
-import "../../../public/css/font-awesome/css/all.css";
-import "../template/custom.css";
-
-
-
