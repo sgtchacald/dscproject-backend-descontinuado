3490939cc44ba6d938d05c0f1105999022cff84e
Diego Cordeiro <sgt.chacal.d@gmail.com>
2023-02-23 16:30:08 -0300

    Autenticação: Ajustando mensagem de erro para login inválido.

--- src/main/java/br/com/diegocordeiro/dscproject/resources/AuthResource.java
+++ src/main/java/br/com/diegocordeiro/dscproject/resources/AuthResource.java
@@ -1,5 +1,6 @@
 package br.com.diegocordeiro.dscproject.resources;
 
+import br.com.diegocordeiro.dscproject.domain.Usuario;
 import br.com.diegocordeiro.dscproject.dto.CredenciaisDTO;
 import br.com.diegocordeiro.dscproject.dto.EmailDTO;
 import br.com.diegocordeiro.dscproject.dto.TokenJWT;
@@ -7,6 +8,8 @@ import br.com.diegocordeiro.dscproject.security.JWTUtil;
 import br.com.diegocordeiro.dscproject.security.UserSS;
 import br.com.diegocordeiro.dscproject.services.AuthService;
 import br.com.diegocordeiro.dscproject.services.UsuarioService;
+import br.com.diegocordeiro.dscproject.services.exceptions.AuthorizationException;
+import br.com.diegocordeiro.dscproject.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,22 +33,25 @@ public class AuthResource {
 
 	@PostMapping
 	public ResponseEntity efetuarLogin(@RequestBody @Valid CredenciaisDTO credenciais, HttpServletResponse response) {
+		try {
+			UserSS usuario = new UserSS();
 
-		UserSS usuario = new UserSS();
+			usuario.setLogin(credenciais.getLogin());
+			usuario.setSenha(credenciais.getSenha());
 
-		usuario.setLogin(credenciais.getLogin());
-		usuario.setSenha(credenciais.getSenha());
+			UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(usuario.getLogin(), usuario.getSenha());
 
-		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(usuario.getLogin(), usuario.getSenha());
+			Authentication authentication = manager.authenticate(authenticationToken);
 
-		Authentication authentication = manager.authenticate(authenticationToken);
+			String token = jwtUtil.gerarToken(usuario);
 
-		String token = jwtUtil.gerarToken(usuario);
+			response.addHeader("Authorization", "Bearer " + token);
+			response.addHeader("access-control-expose-headers", "Authorization");
 
-		response.addHeader("Authorization", "Bearer " + token);
-		response.addHeader("access-control-expose-headers", "Authorization");
-
-		return ResponseEntity.ok(new TokenJWT(token));
+			return ResponseEntity.ok(new TokenJWT(token));
+		}catch (Exception e){
+			throw new AuthorizationException("Erro, login e/ou senha inválidos.");
+		}
 	}
 
 	@RequestMapping(value = "/atualizar-token", method = RequestMethod.POST)
