a53e599eee2ca8e0eb95c9402cd3cbf39bea84d2
Diego Cordeiro <40904257+sgtchacald@users.noreply.github.com>
2023-09-21 17:07:51 -0300

    Update substituidorProperties.sh

--- substituidorProperties.sh
+++ substituidorProperties.sh
@@ -3,10 +3,10 @@
 # Substituindo os marcadores de posição pelos valores dos secrets
 
 #Dados de Conexão do banco de dados
-sed -i "s/SPRING_DATASOURCE_URL/${{ secrets.SPRING_DATASOURCE_URL }}/g" src/main/resources/application-prod.properties
-sed -i "s/SPRING_DATASOURCE_USERNAME/${{ secrets.SPRING_DATASOURCE_USERNAME }}/g" src/main/resources/application-prod.properties
-sed -i "s/SPRING_DATASOURCE_PASSWORD/${{ secrets.SPRING_DATASOURCE_PASSWORD }}/g" src/main/resources/application-prod.properties
+sed -i "s|spring.datasource.url=.*|spring.datasource.url=${SPRING_DATASOURCE_URL}|" src/main/resources/application-prod.properties
+sed -i "s|spring.datasource.url=.*|spring.datasource.url=${SPRING_DATASOURCE_USERNAME}|" src/main/resources/application-prod.properties
+sed -i "s|spring.datasource.url=.*|spring.datasource.url=${SPRING_DATASOURCE_PASSWORD}|" src/main/resources/application-prod.properties
 
 #Dados de Conexão de envio de email
-sed -i "s/SPRING_MAIL_PASSWORD/${{ secrets.SPRING_MAIL_PASSWORD }}/g" src/main/resources/application-prod.properties
-sed -i "s/SPRING_MAIL_USERNAME/${{ secrets.SPRING_MAIL_USERNAME }}/g" src/main/resources/application-prod.properties
+sed -i "s|spring.datasource.url=.*|spring.datasource.url=${SPRING_MAIL_PASSWORD}|" src/main/resources/application-prod.properties
+sed -i "s|spring.datasource.url=.*|spring.datasource.url=${SPRING_MAIL_USERNAME}|" src/main/resources/application-prod.properties
