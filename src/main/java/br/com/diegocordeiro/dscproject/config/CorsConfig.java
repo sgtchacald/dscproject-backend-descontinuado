2e00d07b866ad352e6e1ddaea4a609b8bc073b53
Diego Cordeiro <sgt.chacal.d@gmail.com>
2023-09-25 13:18:34 -0300

    Arquitetura: Alteração para subir pacote para produção

--- src/main/java/br/com/diegocordeiro/dscproject/config/CorsConfig.java
+++ src/main/java/br/com/diegocordeiro/dscproject/config/CorsConfig.java
@@ -32,6 +32,9 @@ public class CorsConfig implements WebMvcConfigurer{
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry
+            .addMapping("/**")
+            .allowedOrigins("https://diegocordeiro.com.br/")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
